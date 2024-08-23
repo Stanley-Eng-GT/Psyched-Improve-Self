534f920e6d9df09e7fab4157562172056d35ed9f
Stanley-Eng-GT <stanley.eng.gt@gmail.com>
2024-08-23 11:38:04 +1000

    Initial Commit

---
+++ Psyched Improve Self/RulesStruc.swift
@@ -0,0 +1,19 @@
+//
+//  RulesStruc.swift
+//  Psyched Improve Self
+//
+//  Created by Stanley Eng on 19/08/2024.
+//
+
+import Foundation
+
+
+struct RulesStruc {
+    var rule: String
+    var ruleDescription: String
+}
+
+struct ProblemStruc {
+    var problem: String
+    var jsonFileName: String
+}
---
+++ Psyched Improve Self/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Psyched Improve Self
+//
+//  Created by Stanley Eng on 9/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+//    val jsonString = {
+//        "How to be happy": [
+//            "1) Seek to build your own character",
+//            "2) Stop seeking the praise of other people",
+//            "3) Focus on what you have control over and ignore the rest",
+//            "4) Do something productive with your anger ",
+//            "5) Negative emotions are a result of negative thinking",
+//            "6) Be prepared for any outcome and act with resilience ",
+//            "7) Be insanely grateful for what you have",
+//            "8) Tolerance is in not judging",
+//            "9) The peace you need is in you",
+//            "10) The peace you need is in you",
+//        ]};
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .systemBlue
+        
+        
+        
+    }
+}
